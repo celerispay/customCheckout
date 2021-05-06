baf754de4a5c40d198ea5f3aa476cdf5b7522bce
Charchit Khandelwal <charchit@newgenpayments.com>
2021-05-06 16:16:34 +0530

    [Update] save invoice email api js
    Signed-off-by: Charchit Khandelwal <charchit@newgenpayments.com>

---
+++ view/frontend/web/js/action/save-invoice-email.js
@@ -0,0 +1,57 @@
+define(
+    [
+        'jquery',
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/model/url-builder',
+        'Magento_Checkout/js/model/error-processor',
+        'mage/url'
+    ],
+    function($, quote, urlBuilder, errorProcessor, urlFormatter) {
+        'use-strict';
+
+        return {
+
+            save: function(email) {
+                if (email) {
+                    var quoteId = quote.getQuoteId();
+                    var url;
+
+                    apiUrl = urlBuilder.createUrl('/guest-carts/:cartId/set-invoice-email', { cartId: quoteId });
+
+                    var payload = {
+                        cartId: quoteId,
+                        checkoutInvoiceEmail: {
+                            checkoutInvoiceEmail: email
+                        }
+                    };
+
+                    if (!payload.checkoutInvoiceEmail.checkoutInvoiceEmail) {
+                        return true;
+                    }
+                    var result = true;
+
+                    $.ajax({
+                        url: urlFormatter.build(apiUrl),
+                        data: JSON.stringify(payload),
+                        global: false,
+                        contentType: 'application/json',
+                        type: 'PUT',
+                        async: false
+                    }).done(
+                        function(response) {
+                            console.log(response);
+                            result = true;
+                        }
+                    ).fail(
+                        function(response) {
+                            console.log(response);
+                            result = false;
+                            errorProcessor.process(response);
+                        }
+                    );
+                    return result;
+                }
+            }
+        }
+    }
+)
