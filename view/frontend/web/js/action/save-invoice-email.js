f6ef13ec1f942f5b6086d2be736482ef07326cd3
Charchit Khandelwal <charchit@newgenpayments.com>
2021-05-13 18:11:49 +0530

    [Update] Invoice check customer status loggin
    Signed-off-by: Charchit Khandelwal <charchit@newgenpayments.com>

--- view/frontend/web/js/action/save-invoice-email.js
+++ view/frontend/web/js/action/save-invoice-email.js
@@ -2,11 +2,12 @@ define(
     [
         'jquery',
         'Magento_Checkout/js/model/quote',
+        'Magento_Customer/js/model/customer',
         'Magento_Checkout/js/model/url-builder',
         'Magento_Checkout/js/model/error-processor',
         'mage/url'
     ],
-    function($, quote, urlBuilder, errorProcessor, urlFormatter) {
+    function($, quote, customer, urlBuilder, errorProcessor, urlFormatter) {
         'use-strict';
 
         return {
@@ -14,9 +15,14 @@ define(
             save: function(email) {
                 if (email) {
                     var quoteId = quote.getQuoteId();
+                    var isCustomer = customer.isLoggedIn();
                     var url;
 
-                    apiUrl = urlBuilder.createUrl('/guest-carts/:cartId/set-invoice-email', { cartId: quoteId });
+                    if (isCustomer) {
+                        apiUrl = urlBuilder.createUrl('/carts/mine/set-invoice-email', {});
+                    } else {
+                        apiUrl = urlBuilder.createUrl('/guest-carts/:cartId/set-invoice-email', { cartId: quoteId });
+                    }
 
                     var payload = {
                         cartId: quoteId,
