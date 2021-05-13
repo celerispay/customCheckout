8072be20e5b80b6688bf19a7b78a6cfc5aa96c92
Charchit Khandelwal <charchit@newgenpayments.com>
2021-05-13 18:05:15 +0530

    [Update] extend checkout shipping js
    Signed-off-by: Charchit Khandelwal <charchit@newgenpayments.com>

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/shipping': {
+                'Boostsales_CustomCheckout/js/view/shipping-mixin': true
+            }
+        }
+    }
+};
