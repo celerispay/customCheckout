edb8ffd99a074eb67c52a2120657f4fd9d354afd
Charchit Khandelwal <charchit@celerispay.com>
2021-11-18 19:01:01 +0530

    [Update][Cleanup] checkout set shipping method
    Signed-off-by: Charchit Khandelwal <charchit@celerispay.com>

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,9 +3,6 @@ var config = {
         mixins: {
             'Magento_Checkout/js/view/shipping': {
                 'Boostsales_CustomCheckout/js/view/shipping-mixin': true
-            },
-            'Magento_Checkout/js/model/checkout-data-resolver': {
-                'Boostsales_CustomCheckout/js/view/checkout-data-resolver-mixin': true
             }
         }
     }
