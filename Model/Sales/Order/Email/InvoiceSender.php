20bf336016abea0f567be05674704ac4425b1dd1
Charchit Khandelwal <charchit@newgenpayments.com>
2022-01-12 19:24:06 +0530

    [Update] DI and invoiceSender construct
    Signed-off-by: Charchit Khandelwal <charchit@newgenpayments.com>

--- Model/Sales/Order/Email/InvoiceSender.php
+++ Model/Sales/Order/Email/InvoiceSender.php
@@ -5,17 +5,65 @@
  */
 namespace Boostsales\CustomCheckout\Model\Sales\Order\Email;
 
+use Magento\Framework\DataObject;
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Payment\Helper\Data as PaymentHelper;
 use Magento\Sales\Model\Order;
-use Magento\Sales\Model\Order\Email\Sender\InvoiceSender as MageInvoiceSender;
-
+use Magento\Sales\Model\Order\Address\Renderer;
+use Magento\Sales\Model\Order\Email\Container\InvoiceIdentity;
+use Magento\Sales\Model\Order\Email\Container\Template;
+use Magento\Sales\Model\Order\Email\Sender;
+use Magento\Sales\Model\Order\Invoice;
+use Magento\Sales\Model\ResourceModel\Order\Invoice as InvoiceResource;
 /**
  * Sends order invoice email to the customer.
  *
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
-class InvoiceSender extends MageInvoiceSender
+class InvoiceSender extends \Magento\Sales\Model\Order\Email\Sender\InvoiceSender
 {
+        /**
+     * @var PaymentHelper
+     */
+    protected $paymentHelper;
 
+    /**
+     * @var InvoiceResource
+     */
+    protected $invoiceResource;
+
+    /**
+     * Global configuration storage.
+     *
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $globalConfig;
+
+    /**
+     * @var Renderer
+     */
+    protected $addressRenderer;
+
+    /**
+     * Application Event Dispatcher
+     *
+     * @var ManagerInterface
+     */
+    protected $eventManager;
+
+    public function __construct(
+        Template $templateContainer,
+        InvoiceIdentity $identityContainer,
+        \Magento\Sales\Model\Order\Email\SenderBuilderFactory $senderBuilderFactory,
+        \Psr\Log\LoggerInterface $logger,
+        Renderer $addressRenderer,
+        PaymentHelper $paymentHelper,
+        InvoiceResource $invoiceResource,
+        \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig,
+        ManagerInterface $eventManager
+    ) {
+        parent::__construct($templateContainer, $identityContainer, $senderBuilderFactory, $logger, $addressRenderer, $paymentHelper, $invoiceResource, $globalConfig,$eventManager);
+    }
     /**
      * Populate order email template with customer information.
      *
@@ -38,6 +86,7 @@ class InvoiceSender extends MageInvoiceSender
         }else{
             $customerEmail = $order->getCustomerEmail();
         }
+        $this->logger->info('this is it');
         $this->identityContainer->setCustomerName($customerName);
         $this->identityContainer->setCustomerEmail($customerEmail);
         $this->templateContainer->setTemplateId($templateId);
